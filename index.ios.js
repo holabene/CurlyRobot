9f9c8b182316734e9746f1441909f20be57d54b7
Benedict <benedict@phpdevel.net>
2017-04-03 18:53:24 +0200

    Edit welcome screen

--- index.ios.js
+++ index.ios.js
@@ -17,10 +17,10 @@ export default class CurlyRobot extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          Hello! I'm CurlyRobot
         </Text>
         <Text style={styles.instructions}>
-          To get started, edit index.ios.js
+          My first app with React Native
         </Text>
         <Text style={styles.instructions}>
           Press Cmd+R to reload,{'\n'}
